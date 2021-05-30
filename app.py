3e47f4ab1a2495c54a2396bdf8258e48710de50a
Carlos Rivero <carlosrivero1988@gmail.com>
2021-05-30 12:37:32 +0200

    Añadida referencia para peliculas populares, basado en el ejercicio prueba2

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, abort, request
 import os, json, requests
 
 app= Flask (__name__)
+url_base="https://api.themoviedb.org/3/"
 
 
 ## PROGRAMA ##
@@ -12,9 +13,14 @@ app= Flask (__name__)
 def inicio():
     return render_template("index.html")
 
+@app.route('/peliculaspopulares', methods=["GET"])
+def populares():
+    parametros={"api_key":key,"language":'es-ES',"page":1}
+    r=requests.get(url_base+"movie/popular",params=parametros)
+    if r.status_code==200:
+        documento=r.json()
+    return render_template("pelispopulares.html",)
 
-
-
-
+key=os.environ["key"]
 port= os.environ["PORT"]
 app.run('0.0.0.0',int(port),debug=False)
