ea81692458e14947eb861503c7391d3eb4c08702
Carlos Rivero <carlosrivero1988@gmail.com>
2021-05-30 10:35:40 +0200

    Añadido programa base con líneas básicas de código

---
+++ app.py
@@ -0,0 +1,15 @@
+## IMPORTAR PAQUETES ##
+
+from flask import Flask, render_template, abort, requests
+import os, json
+
+
+
+
+
+
+
+
+
+port= os.environ["PORT"]
+app.run('0.0.0.0',int(port),debug=False)
