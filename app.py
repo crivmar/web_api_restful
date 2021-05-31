aa3d91b86caf970d149095192ef23560cbe212b2
Carlos Rivero <carlosrivero1988@gmail.com>
2021-05-31 10:46:43 +0200

    Probando una nueva ruta para generar un 'siguiente'

--- app.py
+++ app.py
@@ -15,7 +15,29 @@ def inicio():
 
 @app.route('/peliculaspopulares', methods=["GET"])
 def populares():
-    parametros={"api_key":key,"language":'es-ES',"page":1}
+    page=1
+    parametros={"api_key":key,"language":'es-ES',"page":page}
+    r=requests.get(url_base+"movie/popular",params=parametros)
+    listado=[]
+    if r.status_code==200:
+        documento=r.json()
+        for i in documento.get("results"):
+            diccionario={}
+            if i.get("title"):
+                diccionario["nombre"]=i.get("title")
+            else:
+                diccionario["nombre"]=i.get("original_title")
+            diccionario["id"]=i.get("id")
+            listado.append(diccionario)
+        pagina=documento.get("page")
+        total=documento.get("total_pages")
+        page=+1
+    return render_template("pelispopulares.html",listado=listado,pagina=pagina,total=total)
+
+@app.route('/peliculaspopulares/<int:page>', methods=["GET"])
+def populares(page):
+    page=page
+    parametros={"api_key":key,"language":'es-ES',"page":page}
     r=requests.get(url_base+"movie/popular",params=parametros)
     listado=[]
     if r.status_code==200:
@@ -31,6 +53,7 @@ def populares():
         pagina=documento.get("page")
         total=documento.get("total_pages")
     return render_template("pelispopulares.html",listado=listado,pagina=pagina,total=total)
+
 
 key=os.environ["KEY"]
 port= os.environ["PORT"]
